7ea8e513a59f0eb8a856506d7f3158e1c0819b2a
Max Phillips <max@maxphillipsdev.com>
2021-09-17 14:31:01 +1000

    Remove boilerplate

--- web/src/components/app.tsx
+++ web/src/components/app.tsx
@@ -1,23 +1,11 @@
-import { FunctionalComponent, h } from 'preact';
-import { Route, Router } from 'preact-router';
-
-import Home from '../routes/home';
-import Profile from '../routes/profile';
-import NotFoundPage from '../routes/notfound';
-import Header from './header';
+import { FunctionalComponent, h } from "preact";
 
 const App: FunctionalComponent = () => {
-    return (
-        <div id="preact_root">
-            <Header />
-            <Router>
-                <Route path="/" component={Home} />
-                <Route path="/profile/" component={Profile} user="me" />
-                <Route path="/profile/:user" component={Profile} />
-                <NotFoundPage default />
-            </Router>
-        </div>
-    );
+  return (
+    <div id="preact_root">
+      <p>Hello world!</p>
+    </div>
+  );
 };
 
 export default App;
