27e30d9ca1f8ef5972a5dc00433706b836880631
Max Phillips <max@maxphillipsdev.com>
2021-09-17 17:08:49 +1000

    Fixed a bunch of issues

--- worker/index.ts
+++ worker/index.ts
@@ -1,11 +1,8 @@
-import { Client, Intents } from "discord.js";
+import { Client, Intents, TextChannel } from "discord.js";
 import { config } from "dotenv";
 import express from "express";
 
 import morgan from "morgan";
-
-// Load morgan
-const logger = morgan("tiny");
 
 // Load .env
 config();
@@ -21,12 +18,40 @@ bot.login(process.env.DISCORD_TOKEN);
 // Setup express server
 const api = express();
 
+// Middleware
+api.use(express.json());
+api.use(express.urlencoded({ extended: true }));
+api.use(morgan("tiny"));
+
 api.get("/guilds", (req, res) => {
   const guilds = bot.guilds.cache.map((guild) => guild.id);
   res.send(guilds);
 });
 
-api.post("/:guildID/channelID/message", (req, res) => {});
+api.get("/guilds/:guildID/channels", (req, res) => {
+  const channels = bot.guilds.cache.get(req.params.guildID)?.channels.cache;
+  res.send(
+    channels?.map((channel) => {
+      return {
+        id: channel.id,
+        name: channel.name,
+      };
+    })
+  );
+});
+
+api.post("/guilds/:guildID/channels/:channelID/message", (req, res) => {
+  // const guild = bot.guilds.cache.get(req.params.guildID)
+  const channel = bot.channels.cache.get(req.params.channelID) as TextChannel;
+  try {
+    channel.send(req.body.message);
+    console.log(JSON.stringify(req.body));
+    res.sendStatus(200);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
 
 api.listen(process.env.PORT, () => {
   console.log(`Express server listening on port ${process.env.PORT}`);
